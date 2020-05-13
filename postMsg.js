fd211e36a69fa372f06ca1f38a81c9ef5301d208
uiuihby <1350285537@qq.com>
2020-05-13 16:33:08 +0800

    ~

--- postMsg.js
+++ postMsg.js
@@ -3,38 +3,17 @@ var info = require("./config");
 var api = new wechatAPI(info.appid, info.appsecret);
 var superagent = require('superagent')
 const schedule = require('node-schedule')
-const Rule2 = new schedule.RecurrenceRule()
-Rule2.hour = [14]
-Rule2.minute = [55]
+
+
 var sendmessage = function () {                   // 传值给我
 
     var openid = 'ouRXww357-Uu8UtEnjXW2yLsrrfk'
     var templateId = 'gPxkidhbj35uH6wibpLfp9TbvEIYmBwJHsQuKrXuQjg';
     var url = 'http://weixin.qq.com/download';
 
-    const Rule1 = new schedule.RecurrenceRule()
-    Rule1.hour = [20]
-    Rule1.minute = [30]
-    schedule.scheduleJob(Rule1, function () {
-        var data = {
-            remark: {
-                value: "别忘记看咸鱼!!",
-                "color": "#000"
-            }
-        };
 
-        
-        api.sendTemplate(openid, "jzQyAWx1S9wEuE3wNRjCSr-WxVzahJ2VVAoT2dtuEMk", url, data, function (err, result) {
-            if (err) {
-                console.log('err');
-            } else {
-                console.log(result);
-            }
-        }); 
-
-    }),
-    schedule.scheduleJob(Rule2, function () {
-        // 每天14点/55分
+    schedule.scheduleJob("1 55 14 * * *", function () {
+        // 每天14点/55分/1s
         var getDp = getDap();
         getDp.then(function (dataD) { // 如果AJAX成功，获得响应内容
             let color = "#CD0"
@@ -66,21 +45,23 @@ var sendmessage = function () {                   // 传值给我
                 }
             };
             if (dataD['10'] * 1 < 2700) {
-                data.remark.value = "2000左右处于绝对低位非常非常建议购买"
+                data.remark.value = "2700左右处于绝对低位非常非常建议购买 80%"
             }else if (dataD['10'] * 1 < 2750) {
-                data.remark.value = "2750左右处于低位建议购买"
+                data.remark.value = "2750左右处于低位建议购买 40%"
             } else if (dataD['10'] * 1 < 2800) {
-                data.remark.value = "2800可以购买"
-            } else if (dataD['10'] * 1 < 2850) {
-                data.remark.value = "2850一般般吧观望"
+                data.remark.value = "2800左右可以购买20%"
             } else if (dataD['10'] * 1 < 2900) {
-                data.remark.value = "建议卖出20%"
+                data.remark.value = "2850左右一般般吧观望"
+            } else if (dataD['10'] * 1 < 2950) {
+                data.remark.value = "2950左右建议卖出20%"
             }else if (dataD['10'] * 1 < 3000) {
-                data.remark.value = "建议卖出30%"
+                data.remark.value = "3000左右建议卖出25%"
+            }else if (dataD['10'] * 1 < 3050) {
+                data.remark.value = "3050左右建议卖出30%"
             }else if (dataD['10'] * 1 < 3100) {
-                data.remark.value = "建议卖出50%"
-            }else if (dataD['10'] * 1 > 3100) {
-                data.remark.value = "建议卖出100%"
+                data.remark.value = "3100左右建议卖出40%"
+            }else if (dataD['10'] * 1 > 3150) {
+                data.remark.value = "高于3150建议卖出100%"
             }else {
                 data.remark.value = "特殊价格自己想想吧"
             }
